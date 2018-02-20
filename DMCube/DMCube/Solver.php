a045aa3270c81d92c5612d18f4fb8e534ef3e7d2
dmnmlk <33606665+dmnmlk@users.noreply.github.com>
2018-02-20 17:53:08 +0100

    Add files via upload

---
+++ DMCube/DMCube/Solver.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'Move.php';
+
+/**
+ * Undocumented class
+ */
+class Solver
+{
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function solveCube($cube)
+	{
+		
+	}
+}
